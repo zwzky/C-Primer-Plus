801e72f186110fa35affecc7efba5d4d3bf7cf6e
zwzky <zwz1689458834@gmail.com>
2020-09-01 00:43:04 +0800

    add manybooks.c

---
+++ Ch14/笔记/manybooks.c
@@ -0,0 +1,17 @@
+/* manybooks.c -- 包括多本书的图书目录 */
+
+#include <stdio.h>
+#incldue <string.h>
+
+char * s_gets(char * st, int n)
+
+#define MAXTITL 40
+#define MAXAUTL 40
+#define MAXBKS  100
+
+struct book {
+    char title[MAXTITL];
+
+
+
+}
