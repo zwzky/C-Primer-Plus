f8f0c7a394fa6322624a5bc4ccba6547d5c53ea8
zwzky <zwz1689458834@gmail.com>
2020-09-02 19:53:29 +0800

    modify manybooks.c

--- Ch14/笔记/manybooks.c
+++ Ch14/笔记/manybooks.c
@@ -12,6 +12,24 @@ char * s_gets(char * st, int n)
 struct book {
     char title[MAXTITL];
     char author[
+    float value;
+};
+
+int main(void)
+{
+    struct book library[MAXBKS];
+    int count = 0;
+    int index;
+
+    printf("Please enter the book title.\n");
+    printf("Press [Enter] at the start of a line to stop.\n");
+    while(count < MAXBKS && s_gets(library[count].title, MAXTITL) != NULL
+         && library[count].title[0] != '\0')
+    {
+        printf("Now enter the author.\n");
+
+
+    }
 
 
 }
