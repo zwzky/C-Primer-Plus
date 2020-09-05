d1fcce31d4a86056f374516f0cb3f067d9d06d18
kongyuan <zwz1689458834@gmail.com>
2020-09-05 14:42:47 +0800

    add func_ptr.c

---
+++ Ch14/笔记/func_ptr.c
@@ -0,0 +1,16 @@
+// func_ptr.c -- 使用函数指针
+
+#include <stdio.h>
+#include <string.h>
+@include <ctype.>
+
+#define LEN  81
+
+char * s_gets(char * st, int n);
+char showmenu(void);
+char eatline(void);
+
+
+
+
+}
